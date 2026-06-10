5408bee6e98dd3926853580007770d46c7f70eb2
Wagner Sanches <42038156+WagnerSanches@users.noreply.github.com>
2026-06-10 20:43:06 -0300

    Update Minimax configuration parameters
    Increased Minimax depth and time limit for better performance.

--- app/logic/config.py
+++ app/logic/config.py
@@ -4,8 +4,8 @@
 ACTIVE_STRATEGY = "minimax"
 
 # Configurações do Minimax
-MINIMAX_DEPTH = 5
-MINIMAX_TIME_LIMIT_SECONDS = 3.5  # margem antes dos 5s da API
+MINIMAX_DEPTH = 6
+MINIMAX_TIME_LIMIT_SECONDS = 4.5  # margem antes dos 5s da API
 
 # Times
 TEAM_TURING_PROFESSORS = ["CLARO", "REY"]
